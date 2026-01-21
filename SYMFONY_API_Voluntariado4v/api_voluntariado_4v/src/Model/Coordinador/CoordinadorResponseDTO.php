1acd9835afc634e22fa5ac4667d87a860c92e8df
Ivan <ivancarrillovela@gmail.com>
2026-01-21 23:48:52 +0100

    fix: Añadir atributos al CordinadorResponseDTO para mandar la información necesaria al front end

--- SYMFONY_API_Voluntariado4v/api_voluntariado_4v/src/Model/Coordinador/CoordinadorResponseDTO.php
+++ SYMFONY_API_Voluntariado4v/api_voluntariado_4v/src/Model/Coordinador/CoordinadorResponseDTO.php
@@ -7,8 +7,10 @@ use App\Entity\Coordinador;
 class CoordinadorResponseDTO
 {
     public function __construct(
-        public int $id, // ID del Usuario
-        public string $nombre,
+        public int $id,
+        public ?string $nombre,
+        public ?string $apellidos,
+        public ?string $telefono,
         public string $rol,
         public string $correo,
         public string $estado_cuenta
@@ -18,7 +20,9 @@ class CoordinadorResponseDTO
     {
         return new self(
             $coord->getUsuario()->getId(),
-            $coord->getNombre() . ' ' . $coord->getApellidos(),
+            $coord->getNombre(),
+            $coord->getApellidos(),
+            $coord->getTelefono(),
             $coord->getUsuario()->getRol()->getNombre(),
             $coord->getUsuario()->getCorreo(),
             $coord->getUsuario()->getEstadoCuenta()
